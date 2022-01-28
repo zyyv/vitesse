1712b417034016a3fe7f8e4026763d0cb6ef9c32
chris-zhu <1633711653@qq.com>
2022-01-28 16:24:26 +0800

    fix: resolve warning

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -6,40 +6,42 @@ const { name } = storeToRefs(useUserStore())
 </script>
 
 <template>
-  <header f-c justify-end py-2 px-5>
-    <Navlink to="https://github.com/chris-zhu/modele">
+  <div>
+    <header f-c justify-end py-2 px-5>
+      <Navlink to="https://github.com/chris-zhu/modele">
+        <div
+          hover="text-lightblue cursor-pointer"
+          text-black-800
+          w-6
+          h-6
+          mr-5
+          class="i-carbon:logo-github"
+        />
+      </Navlink>
       <div
-        hover="text-lightblue cursor-pointer"
         text-black-800
+        hover="text-lightblue cursor-pointer"
         w-6
         h-6
-        mr-5
-        class="i-carbon:logo-github"
+        class="i-carbon:language"
+        @click="toggleLocales"
       />
-    </Navlink>
-    <div
-      text-black-800
-      hover="text-lightblue cursor-pointer"
-      w-6
-      h-6
-      class="i-carbon:language"
-      @click="toggleLocales"
-    />
-  </header>
-  <div b-1 p-4 f-c>
-    <HelloWorld :msg="t('hello.msg')" />
-  </div>
-  <div f-c-c>
-    <p mt-10 text="xl center" font-lobster>
-      {{ t("intro.desc") }}
-    </p>
-    <Navlink to="/about">
-      <button mt-10 px-4 py-2 text-blue>
-        {{ t("button.about") }}
-      </button>
-    </Navlink>
-    <p mt-10>
-      pinia userStore: <input v-model="name">
-    </p>
+    </header>
+    <div b-1 p-4 f-c>
+      <HelloWorld :msg="t('hello.msg')" />
+    </div>
+    <div f-c-c>
+      <p mt-10 text="xl center" font-lobster>
+        {{ t("intro.desc") }}
+      </p>
+      <Navlink to="/about">
+        <button mt-10 px-4 py-2 text-blue>
+          {{ t("button.about") }}
+        </button>
+      </Navlink>
+      <p mt-10>
+        pinia userStore: <input v-model="name">
+      </p>
+    </div>
   </div>
 </template>
