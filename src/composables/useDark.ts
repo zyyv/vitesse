80b131cacbacb3137dbdb927cb282c85f944b9da
chris-zhu <1633711653@qq.com>
2022-01-31 14:14:25 +0800

    fix

--- src/composables/useDark.ts
+++ src/composables/useDark.ts
@@ -1,2 +1,3 @@
 export const isDark = useDark()
-export const toggleDark = useToggle(isDark)
+// export const toggleDark = useDebounceFn(useToggle(isDark), 200)
+export const toggleDark = useThrottleFn(() => { useToggle(isDark)() }, 2000)
